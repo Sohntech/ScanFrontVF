419ef9ddd28f1332feed4fcdd5cae28f3eaffc0c
Sohntech <ndiagalo259@gmail.com>
2025-02-17 09:57:04 +0000

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://sascan2.onrender.com',
         changeOrigin: true,
       },
     },
