453c589f6f5ecbc568275c3344fba22d9c685948
Sohntech <ndiagalo259@gmail.com>
2025-02-17 10:13:21 +0000

    fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -18,4 +17,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  define: {
+    'process.env': {},
+  }
 })
