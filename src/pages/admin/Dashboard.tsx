1181071954a49816ac705012470cab9c3d3393d8
Sohntech <ndiagalo259@gmail.com>
2025-02-20 10:27:26 +0000

    feat:tab disabled

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -313,31 +313,29 @@ function AdminDashboard() {
               Vue d'ensemble
             </button>
             <button
-              onClick={() => setSelectedTab('present')}
-              className={`pb-4 text-sm font-medium border-b-2 whitespace-nowrap ${
+              className={`pb-4 text-sm font-medium border-b-2 cursor-default whitespace-nowrap ${
                 selectedTab === 'present'
                   ? 'border-green-500 text-green-600'
-                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  : 'border-transparent text-gray-500 '
               }`}
             >
               Présences
             </button>
             <button
-              onClick={() => setSelectedTab('late')}
-              className={`pb-4 text-sm font-medium border-b-2 whitespace-nowrap ${
+              
+              className={`pb-4 text-sm font-medium border-b-2 cursor-default whitespace-nowrap ${
                 selectedTab === 'late'
                   ? 'border-yellow-500 text-yellow-600'
-                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  : 'border-transparent text-gray-500'
               }`}
             >
               Retards
             </button>
             <button
-              onClick={() => setSelectedTab('absent')}
-              className={`pb-4 text-sm font-medium border-b-2 whitespace-nowrap ${
+              className={`pb-4 text-sm font-medium border-b-2 cursor-default whitespace-nowrap ${
                 selectedTab === 'absent'
                   ? 'border-red-500 text-red-600'
-                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  : 'border-transparent text-gray-500 '
               }`}
             >
               Absences
