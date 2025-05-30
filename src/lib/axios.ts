bf0a6e4d4255bd4f1606ff62c66824e7d58fc9f1
Sohntech <125759801+Sohntech@users.noreply.github.com>
2025-05-30 06:57:38 +0000

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://sascan2.onrender.com/api',
+  baseURL: 'https://sascan2-i5fa.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
